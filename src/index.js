f33e782b96334ab82a3becf344663431be1093b1
Ultral1sk <jderebanoski@gmail.com>
2020-05-10 12:23:59 +0200

    adding css, sass loaders and rewriting bootstrap classes to make changes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 /* this is the entry point where WEBPACK will be sniffing and running everyhing  */
 import { run } from './app/app';
-import "./main.css";
+import "./main.scss";
 import { AlertService } from './app/alert.service';
 import { ComponentService } from './app/component.service';
 
