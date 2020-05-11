fbe5e628453a523e6781e2dce964ef1be0a738fb
Ultral1sk <jderebanoski@gmail.com>
2020-05-11 02:52:29 +0200

    Combining multiple entry points

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,13 +1,12 @@
 const path = require('path');
 const common = require('./webpack.common');
 const merge  = require('webpack-merge');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = merge(common, {
       mode : "production",
-      entry: "./src/index.js",
       output: {
-            filename : "main.[contentHash].js",
+            filename : "[name].[contentHash].bundle.js",
             path: path.resolve(__dirname, "dist")
 
       },
